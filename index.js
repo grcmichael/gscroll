223e4e03a633e07e38deb19b145669788241bfc4
Michael Garcia <garciamichaeljean@gmail.com>
2021-05-21 15:23:22 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,79 @@
+import gsap from "gsap";
+
+export default class GScroll
+{
+	constructor (elmt, speed)
+	{	
+		this.speed = speed/10 || 0.06;
+		this.elmt = elmt;
+		this.isWheeling = null;
+		this.deltaY = 0;
+	}
+
+	init ()
+	{
+		this.current = this.scrollTop = 0;
+
+		this.height = document.querySelector(this.elmt).clientHeight-window.innerHeight;
+		this.deplacement = gsap.quickSetter(this.elmt, "y", "px");
+		
+		this.addTicker = () => {
+	      this.playTicker();
+	    }
+	    gsap.ticker.add(this.addTicker);
+	}
+
+	wheel()
+	{
+		window.addEventListener('wheel', this.ref = (e) => {
+			this.deltaY = e.deltaY;
+			window.clearTimeout( this.isWheeling );
+            this.isWheeling = setTimeout( (e) => {
+                this.deltaY = 0;
+            }, 66);
+
+        });
+	}
+
+	unwheel()
+	{
+		window.removeEventListener('wheel', this.ref);	
+	}
+
+	resize()
+	{
+		this.height = document.querySelector(this.elmt).clientHeight-window.innerHeight;
+	}
+
+	scrollTo(section, dur)
+	{
+		const duration = dur || 1;
+		gsap.to(this, {
+			scrollTop: document.querySelector(section).getBoundingClientRect().top - this.current,
+			duration,
+			ease:'power3.inOut'
+		})
+	}
+
+	playTicker(){
+		const dt = 1.0 - Math.pow(1.0 - this.speed, gsap.ticker.deltaRatio());
+
+		if(this.scrollTop + this.deltaY > this.height){
+			this.scrollTop = this.height;
+        }else if(this.scrollTop + this.deltaY < 0){
+        	this.scrollTop = 0;
+        }else if(this.deltaY !== 0){
+        	this.scrollTop += this.deltaY;
+        }
+
+        this.current += (-this.scrollTop - this.current) * dt;
+        this.deplacement(this.current);
+    }
+
+	destroy()
+	{
+		gsap.killTweensOf(this.elmt);
+		window.removeEventListener('wheel', this.ref);
+		gsap.ticker.remove(this.addTicker);
+	}
+}
